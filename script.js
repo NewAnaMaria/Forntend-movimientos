39b64400eb824e2a2ea57bc53f6d6f7d813ad87b
NewAnaMaria <developeranamaria@gmail.com>
2023-06-09 22:31:52 -0500

    create functions JS

---
+++ script.js
@@ -0,0 +1,17 @@
+const URL_API = "http://localhost:3000/movimientos";
+
+const getMovimientos = async() => {
+    try {
+        const {data} = await axios.get(URL_API);
+        return data;
+        
+    } catch (error) {
+        console.log(error);
+        return[];
+        
+    }
+}
+document.addEventListener("DOMContentLoaded", async()=>{
+    const movimientos = await getMovimientos();
+    console.log(movimientos);
+})
